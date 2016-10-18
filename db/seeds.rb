b582ff0a1d3e004c5675511031a93cc73ab08d8c
Doan Chi Nhat <doan.chi.nhat@framgia.com>
2016-10-18 14:36:53 +0700

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,18 @@ User.create!(name:  "Doan Chi Nhat",
                password:              password,
                password_confirmation: password)
 end
+# Entries
 users = User.order(:created_at).take(6)
 50.times do
 	title = Faker::Lorem.sentence(7)
   content = Faker::Lorem.sentence(7)
   users.each { |user| user.entries.create!(content: content,title: title) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
